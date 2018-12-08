0455eafd91c6d25c15ff41028f5af469ca0d4242
Petr Heinz <p.heinz@email.cz>
2018-12-08 19:24:40 +0100

    layouts

---
+++ DesktopClient/LesniHospodarskaEvidenceView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopClient
+{
+    /// <summary>
+    /// Interakční logika pro LesniHospodarskaEvidenceView.xaml
+    /// </summary>
+    public partial class LesniHospodarskaEvidenceView : Window
+    {
+        public LesniHospodarskaEvidenceView()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
