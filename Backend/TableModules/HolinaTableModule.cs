722ec5386c39f3fc41937987f6df14b788fe7bde
Petr Heinz <p.heinz@email.cz>
2018-12-10 23:03:20 +0100

    holiny

--- Backend/TableModules/HolinaTableModule.cs
+++ Backend/TableModules/HolinaTableModule.cs
@@ -24,5 +24,50 @@ namespace Backend.TableModules
         {
             return db.HolinaTableGateway.SelectAll().Cast<Holina>().ToList().FindAll(x => x.RokVzniku == rokVzniku).ToList();
         }
+
+        public List<string> GetHolinyByYearArrays()
+        {
+            var holiny = LoadHoliny().OrderBy(x => x.RokVzniku).ToList();
+            List<int> years = holiny.Select(x => x.RokVzniku).Distinct().ToList();
+            List<int> counts = new List<int>();
+            foreach (var year in years)
+            {
+                counts.Add(holiny.FindAll(x => x.RokVzniku == year).ToList().Count);
+            }
+
+            List<string> ret = new List<string>();
+            StringBuilder y = new StringBuilder();
+            y.Append("[");
+            var idx = 0;
+            foreach(var year in years)
+            {
+                y.Append(year.ToString());
+                if (idx < years.Count)
+                {
+                    y.Append(",");
+                }
+                idx++;
+            }
+            y.Append("]");
+
+            StringBuilder c = new StringBuilder();
+            c.Append("[");
+            idx = 0;
+            foreach (var count in counts)
+            {
+                c.Append(count.ToString());
+                if (idx < years.Count)
+                {
+                    c.Append(",");
+                }
+                idx++;
+            }
+            c.Append("]");
+
+            ret.Add(y.ToString());
+            ret.Add(c.ToString());
+            return ret;
+
+        }
     }
 }
--- WebApplication/Controllers/GraphController.cs
+++ WebApplication/Controllers/GraphController.cs
@@ -2,14 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.TableDataGateways.StorageContexts;
+using Backend.TableModules;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
 {
     public class GraphController : Controller
     {
+        IStorageContext db;
+        AuthService auth;
+
+        public GraphController(IStorageContext db, AuthService auth)
+        {
+            this.db = db;
+            this.auth = auth;
+        }
+
         public IActionResult Index()
         {
+            if (auth.User == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View();
+        }
+
+        public IActionResult Holiny()
+        {
+            if (auth.User == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var holinaTableModule = new HolinaTableModule(db);
+            var graphData = holinaTableModule.GetHolinyByYearArrays();
+            ViewData["years"] = graphData[0];
+            ViewData["counts"] = graphData[1];
+            return View();
+        }
+
+        public IActionResult Zalesneni()
+        {
+            if (auth.User == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
             return View();
         }
     }
