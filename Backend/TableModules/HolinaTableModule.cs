1f9788cc1bc14760efb63f3e37f2964eb427f1cc
Petr Heinz <p.heinz@email.cz>
2018-12-10 18:49:23 +0100

    map

--- Backend/TableModules/HolinaTableModule.cs
+++ Backend/TableModules/HolinaTableModule.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Backend.Models;
+using Backend.TableDataGateways.StorageContexts;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Backend.TableModules
 {
-    class HolinaTableModule
+    public class HolinaTableModule
     {
+        IStorageContext db;
+        public HolinaTableModule(IStorageContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Holina> LoadHoliny()
+        {
+            return db.HolinaTableGateway.SelectAll().Cast<Holina>().ToList();
+        }
+
+        public List<Holina> LoadHolinyByRokVzniku(int rokVzniku)
+        {
+            return db.HolinaTableGateway.SelectAll().Cast<Holina>().ToList().FindAll(x => x.RokVzniku == rokVzniku).ToList();
+        }
     }
 }
--- WebApplication/Controllers/MapController.cs
+++ WebApplication/Controllers/MapController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.TableDataGateways.StorageContexts;
+using Backend.TableModules;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
 {
     public class MapController : Controller
     {
-        public IActionResult Index()
+        IStorageContext db;
+        AuthService auth;
+
+        public MapController(IStorageContext db, AuthService auth)
         {
+            this.db = db;
+            this.auth = auth;
+        }
+
+        public IActionResult Holiny()
+        {
+            if (auth.User == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var holinaTableModule = new HolinaTableModule(db);
+            ViewData["Holiny"] = holinaTableModule.LoadHoliny();
             return View();
+
         }
     }
 }
